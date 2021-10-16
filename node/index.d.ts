3e23609eb1a2d0a6dc66ccc701aedd29dc428a8e
catamphetamine <kuchumovn@gmail.com>
2021-10-16 15:07:50 +0300

    Fixed TypeScript "typings"

--- node/index.d.ts
+++ node/index.d.ts
@@ -16,8 +16,8 @@ export function parseExcelDate(excelSerialDate: number) : typeof Date;
 
 type Input = Stream | PathLike;
 
-function readXlsxFile(input: Input, options: ParseWithSchemaOptions) : Promise<ParsedObjectsResult>;
-function readXlsxFile(input: Input, options: ParseWithMapOptions) : Promise<ParsedObjectsResult>;
-function readXlsxFile(input: Input, options?: ParseWithoutSchemaOptions) : Promise<Row[]>;
+export function readXlsxFile(input: Input, options: ParseWithSchemaOptions) : Promise<ParsedObjectsResult>;
+export function readXlsxFile(input: Input, options: ParseWithMapOptions) : Promise<ParsedObjectsResult>;
+export function readXlsxFile(input: Input, options?: ParseWithoutSchemaOptions) : Promise<Row[]>;
 
 export default readXlsxFile;
