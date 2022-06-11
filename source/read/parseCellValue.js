79686c59fd80c64009bf3a300755f0bad2f1d71a
catamphetamine <kuchumovn@gmail.com>
2022-06-11 12:17:06 +0300

    Added an optional `reason?: string` property of a with-schema parsing error.

--- source/read/parseCellValue.js
+++ source/read/parseCellValue.js
@@ -3,13 +3,15 @@ import parseDate from './parseDate.js'
 // https://hexdocs.pm/xlsxir/number_styles.html
 const BUILT_IN_DATE_NUMBER_FORMAT_IDS = [14,15,16,17,18,19,20,21,22,27,30,36,45,46,47,50,57]
 
-export default function getCellValue(value, type, {
-	getInlineStringValue,
-	getStyleId,
-	styles,
-	values,
-	properties,
-	options
+// Parses a string `value` of a cell.
+export default function parseCellValue(value, type, {
+  getInlineStringValue,
+  getInlineStringXml,
+  getStyleId,
+  styles,
+  values,
+  properties,
+  options
 }) {
   if (!type) {
     // Default cell type is "n" (numeric).
@@ -24,85 +26,103 @@ export default function getCellValue(value, type, {
   // http://webapp.docx4java.org/OnlineDemo/ecma376/SpreadsheetML/ST_CellType.html
   //
   switch (type) {
-    // If the cell contains formula string.
+    // XLSX tends to store all strings as "shared" (indexed) ones
+    // using "s" cell type (for saving on strage space).
+    // "str" cell type is then generally only used for storing
+    // formula-pre-calculated cell values.
     case 'str':
       value = parseString(value, options)
       break
 
-    // If the cell contains an "inline" (not "shared") string.
+    // Sometimes, XLSX stores strings as "inline" strings rather than "shared" (indexed) ones.
+    // Perhaps the specification doesn't force it to use one or another.
+    // Example: `<sheetData><row r="1"><c r="A1" s="1" t="inlineStr"><is><t>Test 123</t></is></c></row></sheetData>`.
     case 'inlineStr':
       value = getInlineStringValue()
       if (value === undefined) {
-        throw new Error(`Unsupported "inline string" cell value structure`) // : ${cellNode.textContent}`)
+        throw new Error(`Unsupported "inline string" cell value structure: ${getInlineStringXml()}`)
       }
       value = parseString(value, options)
       break
 
-    // If the cell contains a "shared" string.
+    // XLSX tends to store string values as "shared" (indexed) ones.
     // "Shared" strings is a way for an Excel editor to reduce
     // the file size by storing "commonly used" strings in a dictionary
     // and then referring to such strings by their index in that dictionary.
+    // Example: `<sheetData><row r="1"><c r="A1" s="1" t="s"><v>0</v></c></row></sheetData>`.
     case 's':
       // If a cell has no value then there's no `<c/>` element for it.
       // If a `<c/>` element exists then it's not empty.
       // The `<v/>`alue is a key in the "shared strings" dictionary of the
       // XLSX file, so look it up in the `values` dictionary by the numeric key.
-      value = values[Number(value)]
+      const sharedStringIndex = Number(value)
+      if (isNaN(sharedStringIndex)) {
+        throw new Error(`Invalid "shared" string index: ${value}`)
+      }
+      if (sharedStringIndex >= values.length) {
+        throw new Error(`An out-of-bounds "shared" string index: ${value}`)
+      }
+      value = values[sharedStringIndex]
       value = parseString(value, options)
       break
 
+    // Boolean (TRUE/FALSE) values are stored as either "1" or "0"
+    // in cells of type "b".
     case 'b':
-      value = value === '1' ? true : false
+      if (value === '1') {
+        value = true
+      } else if (value === '0') {
+        value = false
+      } else {
+        throw new Error(`Unsupported "boolean" cell value: ${value}`)
+      }
       break
 
-    // Stub: blank stub cell that is ignored by data processing utilities.
+    // XLSX specification seems to support cells of type "z":
+    // blank "stub" cells that should be ignored by data processing utilities.
     case 'z':
       value = undefined
       break
 
-    // Error: `value` is a numeric code.
+    // XLSX specification also defines cells of type "e" containing a numeric "error" code.
+    // It's not clear what that means though.
     // They also wrote: "and `w` property stores its common name".
     // It's unclear what they meant by that.
     case 'e':
       value = decodeError(value)
       break
 
-    // Date: a string to be parsed as a date.
-    // (usually a string in "ISO 8601" format)
+    // XLSX supports date cells of type "d", though seems like it (almost?) never
+    // uses it for storing dates, preferring "n" numeric timestamp cells instead.
+    // The value of a "d" cell is supposedly a string in "ISO 8601" format.
+    // I haven't seen an XLSX file having such cells.
+    // Example: `<sheetData><row r="1"><c r="A1" s="1" t="d"><v>2021-06-10T00:47:45.700Z</v></c></row></sheetData>`.
     case 'd':
       if (value === undefined) {
         break
       }
-      value = new Date(value)
+      const parsedDate = new Date(value)
+      if (isNaN(parsedDate)) {
+        throw new Error(`Unsupported "date" cell value: ${value}`)
+      }
+      value = parsedDate
       break
 
+    // Numeric cells have type "n".
     case 'n':
       if (value === undefined) {
         break
       }
-      value = parseFloat(value)
+      const parsedNumber = Number(value)
+      if (isNaN(parsedNumber)) {
+        throw new Error(`Invalid "numeric" cell value: ${value}`)
+      }
+      value = parsedNumber
       // XLSX does have "d" type for dates, but it's not commonly used.
-      // Instead, spreadsheets prefer using "n" type for dates for some reason.
-      //
-      // In such cases, sometimes a "date" type could be heuristically detected
-      // by looking at such numeric value "format" and seeing if it's a date-specific one.
-      // https://github.com/catamphetamine/read-excel-file/issues/3#issuecomment-395770777
-      //
-      // The list of generic numeric value "formats":
-      // https://xlsxwriter.readthedocs.io/format.html#format-set-num-format
-      //
-      const styleId = getStyleId()
-      if (styleId) {
-        // styleId = parseInt(styleId)
-        const style = styles[styleId]
-        if (!style) {
-          throw new Error(`Cell style not found: ${styleId}`)
-        }
-        if (BUILT_IN_DATE_NUMBER_FORMAT_IDS.indexOf(parseInt(style.numberFormat.id)) >= 0 ||
-          (options.dateFormat && style.numberFormat.template === options.dateFormat) ||
-          (options.smartDateParser !== false && style.numberFormat.template && isDateTemplate(style.numberFormat.template))) {
-          value = parseDate(value, properties)
-        }
+      // Instead, it prefers using "n" type for storing dates as timestamps.
+      if (isDateTimestamp(value, getStyleId(), styles, options)) {
+        // Parse the number as a date timestamp.
+        value = parseDate(value, properties)
       }
       break
 
@@ -205,4 +225,35 @@ function parseString(value, options) {
     value = undefined
   }
   return value
+}
+
+// XLSX does have "d" type for dates, but it's not commonly used.
+// Instead, it prefers using "n" type for storing dates as timestamps.
+//
+// Whether a numeric value is a number or a date timestamp, it sometimes could be
+// detected by looking at the value "format" and seeing if it's a date-specific one.
+// https://github.com/catamphetamine/read-excel-file/issues/3#issuecomment-395770777
+//
+// The list of generic numeric value "formats":
+// https://xlsxwriter.readthedocs.io/format.html#format-set-num-format
+//
+function isDateTimestamp(value, styleId, styles, options) {
+  if (styleId) {
+    const style = styles[styleId]
+    if (!style) {
+      throw new Error(`Cell style not found: ${styleId}`)
+    }
+    if (
+      // Whether it's a "number format" that's conventionally used for storing date timestamps.
+      BUILT_IN_DATE_NUMBER_FORMAT_IDS.indexOf(parseInt(style.numberFormat.id)) >= 0 ||
+      // Whether it's a "number format" that uses a "formatting template"
+      // that the developer is certain is a date formatting template.
+      (options.dateFormat && style.numberFormat.template === options.dateFormat) ||
+      // Whether the "smart formatting template" feature is not disabled
+      // and it has detected that it's a date formatting template by looking at it.
+      (options.smartDateParser !== false && style.numberFormat.template && isDateTemplate(style.numberFormat.template))
+     ) {
+      return true
+    }
+  }
 }
--- test/test.test.js
+++ test/test.test.js
@@ -1,12 +1,12 @@
 import parseXlsx from '../source/read/readXlsxFileNode.js'
 
-const sheetsDir = './test/spreadsheets';
+const sheetsDir = './test/spreadsheets'
 
 const sheets = {
   'excel_mac_2011-basic.xlsx': [ [ 'One', 'Two' ], [ 'Three', 'Four' ] ],
   'excel_mac_2011-formatting.xlsx': [ [ 'Hey', 'now', 'so' ], [ 'cool', null, null ] ],
   'excel_multiple_text_nodes.xlsx': [ [ 'id', 'memo' ], [ 1, 'abc def ghi' ], [ 2, 'pqr stu' ] ]
-};
+}
 
 describe('read-excel-file', function() {
   for (const filename in sheets) {
@@ -16,14 +16,14 @@ describe('read-excel-file', function() {
     (function(filename, expected) {
       describe(filename + ' basic test', function() {
         it('should return the right value', async function() {
-          const result = await parseXlsx(sheetsDir + '/' + filename);
-          expect(result).to.deep.equal(expected);
+          const result = await parseXlsx(sheetsDir + '/' + filename)
+          expect(result).to.deep.equal(expected)
         })
         it('should return the right value with the sheet specified', async function() {
-          const result = await parseXlsx(sheetsDir + '/' + filename, '1');
-          expect(result).to.deep.equal(expected);
+          const result = await parseXlsx(sheetsDir + '/' + filename, '1')
+          expect(result).to.deep.equal(expected)
         })
-      });
-    })(filename, sheets[filename]);
+      })
+    })(filename, sheets[filename])
   }
-});
+})
